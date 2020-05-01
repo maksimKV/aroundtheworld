9fbbb0d40741fe37e5875fd35efc278e55553226
Maksim Kanev <maks_kv@yahoo.com>
2020-05-02 01:46:15 +0300

    Added a custom search menu

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class Map extends Component {
-    state = {
-
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
+
+    state = {
+        startPoint: {
+            long: 0,
+            lat: 0,
+            desc: "",
+        },
+        endPoint: {
+            long: 0,
+            lat: 0,
+            desc: "",
+        }
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        
+        this.setState({
+            startPoint: {
+                long: event.target[0].value,
+                lat: event.target[1].value,
+            }
+        });
+      }
 
     render() {
         return (
+            <React.Fragment>
+
             <LeafletMap
-                center={[50, 10]}
-                zoom={6}
+                center={[this.state.startPoint.long, this.state.startPoint.lat]}
+                zoom={3}
                 attributionControl={true}
                 zoomControl={true}
                 doubleClickZoom={true}
@@ -24,14 +51,29 @@ class Map extends Component {
                     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                 />
 
-                <Marker position={[50, 10]}>
+                <Marker position={[this.state.startPoint.long, this.state.startPoint.lat]}>
                 <Popup>
-                    Popup for any custom information.
+                    {this.state.startPoint.desc}
                 </Popup>
                 </Marker>
             </LeafletMap>
+
+            <div class="startMenu">
+            <h3>Choose location</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label for="long">Longitude</label>
+                    <input type="number" id="long" name="long"/>
+
+                    <label for="lat">Latitude </label>
+                    <input type="number" id="lat" name="lat"/>
+
+                    <input type="submit" value="Search"></input>
+                </form>
+            </div>
+
+            </React.Fragment>
         );
-    }
+    };
 }
 
 export default Map;
